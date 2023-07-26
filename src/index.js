6dd7093f3de03c37d54035a865c423f30612855b
Caique Moreira <caique_moreira_sz@hotmail.com>
2023-07-25 23:51:29 -0300

    fix: controller imported path at index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import listCars from './controllers/listCarsController.js';
+import listCars from './controllers/listCars.js';
 const app = express();
 
 app.use(express.json());
